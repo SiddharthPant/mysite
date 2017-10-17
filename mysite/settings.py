e702a017e71aa98d4908364d4e34f6a7236d638b
Siddharth Pant <spsiddharthpant@gmail.com>
2017-10-17 09:08:17 +0530

    Configure PostgreSQL in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'python_dev',
+        'PASSWORD': 'aromatic',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
